600536e80943091bcf35b7e5f9389496a776828c
Nina Davis <nina.k.davis2@gmail.com>
2021-04-28 18:37:24 -0500

    add loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,14 @@ console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 console.log(enemyNames);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++) {
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyName[i] + " is at " + i + " index");
+}
+
+console.log(enemyNames[3]);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
