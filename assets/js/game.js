c2657244b577eb562183fa019252b0d0061ceb05
Nina Davis <nina.k.davis2@gmail.com>
2021-05-05 14:53:31 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -67,8 +67,13 @@ var fight = function(enemyName) {
     }
   }
 };
-
+// function to start new game
 // fight each enemy-robot by looping over them and fighting them one at a time
+var startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
@@ -93,5 +98,28 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
-// run fight function to start game
-fight();
+// after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+endGame();
+}
+// function to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+}
+// start the game when the page loads
+startGame();
